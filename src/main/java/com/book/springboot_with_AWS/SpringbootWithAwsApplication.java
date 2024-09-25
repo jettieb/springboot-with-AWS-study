e20f39b7264a83275b168ffd6d45e1e22203a034
Bokjihee <bokjihee@naver.com>
2024-09-25 16:33:27 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/book/springboot_with_AWS/SpringbootWithAwsApplication.java
@@ -0,0 +1,13 @@
+package com.book.springboot_with_AWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/springboot_with_AWS/SpringbootWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.springboot_with_AWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
